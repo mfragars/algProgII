9e5d2f0503e300333be9ec993b36d05537e635f5
MAURUCIO FRAGA DE ANDRADE <631010344@fspoa.br>
2019-03-20 20:03:53 -0300

    Exercicio Algoritimos Programação II
    Criação de janela com pygame

--- aula1.py
+++ aula1.py
@@ -1,4 +1,19 @@
 import pygame
+
+pygame.init()
 (width, heigth) = (800, 600)
+window_title = "Oi, sou o PyGame"
+
 screen = pygame.display.set_mode((width, heigth))
-pygame.display.flip()
+
+pygame.display.set_caption(window_title)
+done = False
+
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        done = True
+        
+        pygame.display.flip()
+
+
